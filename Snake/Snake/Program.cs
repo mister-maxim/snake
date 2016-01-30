ce57598a3bb61d7bd3ae00eef2450aabe99c71bc
mister-maxim <makcimushka@gmail.com>
2016-01-30 17:01:25 +0400

    списки из целых чисел

--- Snake/Snake/Program.cs
+++ Snake/Snake/Program.cs
@@ -12,16 +12,25 @@ namespace Snake
         {
  
             Point p1 = new Point(1,3,'x');
-           /* p1.x = 1;
-            p1.y = 3;
-            p1.sym = 'x';*/
             p1.Draw();
 
             Point p2 = new Point(4,5,'#');
-           /* p2.x = 4;
-            p2.y = 5;
-            p2.sym = '#';*/
             p2.Draw();
+
+            List<int> numList = new List<int>();
+            numList.Add(0);
+            numList.Add(1);
+            numList.Add(2);
+
+            int x = numList[0];
+
+            foreach(int i in numList)
+            {
+                Console.WriteLine(i);
+            }
+
+            numList.RemoveAt(0);
+
 
             Console.ReadLine();
         }
