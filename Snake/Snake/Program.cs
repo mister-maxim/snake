0631dcbdfc90d33791cd3d20954683f087130d51
mister-maxim <makcimushka@gmail.com>
2016-01-30 15:32:51 +0400

    class Point содержит лишь данные точки, пока без метода

--- Snake/Snake/Program.cs
+++ Snake/Snake/Program.cs
@@ -13,19 +13,28 @@ namespace Snake
             //Console.WriteLine("Hello world!");
             //Draw(1, 3, '*');
             //Draw(4, 5, '#');
+            /*
             int x1 = 1;
             int y1 = 3;
             char sym1 = 'x';
-            //Console.SetCursorPosition(x1, y1);
-            //Console.Write(sym1);
-            Draw(x1, y1, sym1);
+            Draw(x1, y1, sym1);*/
+            Point p1 = new Point();
+            p1.x = 1;
+            p1.y = 3;
+            p1.sym = 'x';
+            Draw(p1.x, p1.y, p1.sym);
 
+/*
             int x2 = 4;
             int y2 = 5;
             char sym2 = '#';
-            //Console.SetCursorPosition(x2, y2);
-            //Console.Write(sym2);
             Draw(x2, y2, sym2);
+ */
+            Point p2 = new Point();
+            p2.x = 4;
+            p2.y = 5;
+            p2.sym = '#';
+            Draw(p2.x, p2.y, p2.sym);
 
             Console.ReadLine();
         }
