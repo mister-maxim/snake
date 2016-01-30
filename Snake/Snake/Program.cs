16492649b315c6ab302ac3ba995681127094facd
mister-maxim <makcimushka@gmail.com>
2016-01-30 17:07:48 +0400

    список из точек

--- Snake/Snake/Program.cs
+++ Snake/Snake/Program.cs
@@ -17,20 +17,9 @@ namespace Snake
             Point p2 = new Point(4,5,'#');
             p2.Draw();
 
-            List<int> numList = new List<int>();
-            numList.Add(0);
-            numList.Add(1);
-            numList.Add(2);
-
-            int x = numList[0];
-
-            foreach(int i in numList)
-            {
-                Console.WriteLine(i);
-            }
-
-            numList.RemoveAt(0);
-
+            List<Point> pList = new List<Point>();
+            pList.Add(p1);
+            pList.Add(p2);
 
             Console.ReadLine();
         }
